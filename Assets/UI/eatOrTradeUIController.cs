777237b6fab438108f90b432b6a0cd933c1e12ad
Paulmue0 <muellerpaul2000@live.de>
2022-01-08 15:02:18 +0100

    fixed merge conflicts

--- Assets/UI/eatOrTradeUIController.cs
+++ Assets/UI/eatOrTradeUIController.cs
@@ -25,26 +25,6 @@ public class eatOrTradeUIController : UIController
         // Define dialogLabel
         dialogLabel = root.Q<Label>("DialogLabel");
 
-        // Define Components for Attack 1
-        att_1_button = root.Q<Button>("att-1-button");
-        switch_move_description_1 = root.Q<Button>("switch-move-description-1");
-        att_1_description_label = root.Q<Label>("att-1-description-label");
-
-        // Define Components for Attack 2
-        att_2_button = root.Q<Button>("att-2-button");
-        switch_move_description_2 = root.Q<Button>("switch-move-description-2");
-        att_2_description_label = root.Q<Label>("att-2-description-label");
-
-        // Define Components for Attack 3
-        att_3_button = root.Q<Button>("att-3-button");
-        switch_move_description_3 = root.Q<Button>("switch-move-description-3");
-        att_3_description_label = root.Q<Label>("att-3-description-label");
-
-        // Define Components for Attack 4
-        att_4_button = root.Q<Button>("att-4-button");
-        switch_move_description_4 = root.Q<Button>("switch-move-description-4");
-        att_4_description_label = root.Q<Label>("att-4-description-label");
-
         // Define Eat and Trade Button
         eatButton = root.Q<Button>("eat-button");
         tradeButton = root.Q<Button>("trade-button");
